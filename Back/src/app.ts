b5df298ff6fb4cba99dd43a55e0d4c776c07f8ae
RmGkAppDev <roei.meiri9@gmail.com>
2024-12-03 12:40:31 +0200

    Adding notes and proper documentation for the BE app

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -3,44 +3,59 @@ import { answers } from './answers';
 import express from 'express';
 import cors from 'cors';
 
+// Configuring the application
 const app = express();
 const port = 3200;
 
+// Allowing the front to communicate with the server
 const allowedOrigins = ['http://localhost:4200'];
-
 const options: cors.CorsOptions = {
 	origin: allowedOrigins,
 };
-
 app.use(cors(options));
+
+// Configure the application to read the body on the requests.
 app.use(express.json());
 
-let id: number = 0;
+// NOTE: On a real BE, we would keep track of this ID on the data base, 
+// resetting the number for each new conversation
+let messageID: number = 0; 
 
+/**
+ * Test endpoint to test server connectivity
+ */
 app.get('/', (req, res) => {
-	console.log('wow');
+	console.log(`[${new Date().toISOString()}]\tGET Request called from ${req.headers.host}`);
 	res.status(200).json({ message: "Hey there, everything's working fine!" });
 });
 
+/**
+ * Returns result for prompts
+ */
 app.post('/prompt', (req, res) => {
+	console.log(`[${new Date().toISOString()}]\tPOST Request called from ${req.headers.host}`);
+	console.log(`[${new Date().toISOString()}]\tGenerating answer`);
 	const content: string = req.body.content;
 	const answer: Message[] = [
 		{
 			content: content,
-			id: `${id}`,
+			id: `${messageID}`,
 			origin: 'User',
 		},
 		{
 			content: answers[Math.floor(Math.random() * answers.length)],
-			id: `${id++}`,
+			id: `${messageID++}`,
 			origin: 'User',
 		},
 	];
-	id++;
+	messageID++;
 
 	res.status(200).json(answer);
 });
 
+/**
+ * Listens to localhost on port 3200
+ */
 app.listen(port, () => {
 	return console.log(`Express is listening at http://localhost:${port}`);
 });
